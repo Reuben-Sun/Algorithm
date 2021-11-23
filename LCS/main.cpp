bbf7f66e3a59f91ea2a0db65ee8c3f56f6d67fe5
sunzheng <2824016820@qq.com>
2021-11-23 13:23:37 +0800

    最长公共子序列

--- LCS/main.cpp
+++ LCS/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
+int LCS(string text1, string text2){
+    int len1 = text1.length();
+    int len2 = text2.length();
+    vector<vector<int>> dp(len1+1, vector<int>(len2+1));
+    for (int i = 1; i <= len1; i++) {
+        char c1 = text1[i-1];
+        for(int j = 1; j <= len2; j++){
+            char c2 = text2[j-1];
+            if(c1 == c2){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+    return dp[len1][len2];
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string text1 = "abcdef";
+    string text2 = "cde";
+    cout << LCS(text1, text2);
     return 0;
 }
